ea625317ec944e3dace316ce7c765897f20d22cc
Alex Sichitiu <asichitiu@gmail.com>
2024-10-24 20:31:01 -0700

    Added summation of q and a by owner.

--- user_contribution.py
+++ user_contribution.py
@@ -1,34 +1,60 @@
 import sys
 assert sys.version_info >= (3, 5) 
 
-from pyspark.sql import SparkSession, functions, types as t, DataFrameReader
+from pyspark.sql import SparkSession, functions as f, types as t
 
-def main(inputs, output):
+def read_q_and_a_data(input_q, input_a):
     q_schema= t.StructType([
         t.StructField('id', t.StringType()),
         t.StructField('owner_id', t.StringType()),
-        t.StructField('open_timestamp', t.TimestampType()),
+        t.StructField('creation_timestamp', t.TimestampType()),
         t.StructField('close_timestamp', t.TimestampType()),
         t.StructField('score', t.LongType()),
         t.StructField('title', t.StringType()),
         t.StructField('body', t.StringType())
     ])
 
-    qs = spark.read.csv(inputs,
+    q_df = spark.read.csv(input_q,
                         schema=q_schema,
                         header=True,
-                        samplingRatio=0.001,
                         multiLine=True,
                         escape='"',)
     
-    qs.show()
-    print(qs.schema)
+    a_schema = t.StructType([
+        t.StructField('id', t.StringType()),
+        t.StructField('owner_id', t.StringType()),
+        t.StructField('creation_timestamp', t.TimestampType()),
+        t.StructField('parent_id', t.StringType()),
+        t.StructField('score', t.LongType()),
+        t.StructField('body', t.StringType())
+    ])
+
+    a_df = spark.read.csv(input_a,
+                      schema=a_schema,
+                        header=True,
+                        multiLine=True,
+                        escape='"',)
+    
+    return q_df, a_df
+
+def main(input_q, input_a, output):
+    q_df, a_df = read_q_and_a_data(input_q, input_a)
+    q_counts_by_user = q_df.groupBy('owner_id').count().withColumnRenamed('count', 'n_q')
+    a_counts_by_user = a_df.groupBy('owner_id').count().withColumnRenamed('count', 'n_a')
+    qa_counts_by_user = q_counts_by_user.join(a_counts_by_user, 'owner_id')
+    qa_counts_by_user.cache()
+    q_ranking = qa_counts_by_user.orderBy(f.desc('n_q'))
+    a_ranking = qa_counts_by_user.orderBy(f.desc('n_q'))
+    q_ranking.show()
+    a_ranking.show()
+
 
 if __name__ == '__main__':
-    inputs = sys.argv[1]
-    output = sys.argv[2]
+    input_q = sys.argv[1]
+    input_a = sys.argv[2]
+    output = sys.argv[3]
     spark = SparkSession.builder.appName('Final Project: User Contribution').getOrCreate()
     assert spark.version >= '3.0' 
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
-    main(inputs, output)
+    main(input_q, input_a, output)
