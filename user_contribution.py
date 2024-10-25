e97f7d1c3dfa73f9939eb0791cbd8f884778dd70
Alex Sichitiu <asichitiu@gmail.com>
2024-10-25 16:59:31 -0700

    Wrote function to get a percentile breakdown of q's and a's by user percentile.

--- user_contribution.py
+++ user_contribution.py
@@ -1,7 +1,7 @@
 import sys
-assert sys.version_info >= (3, 5) 
+assert sys.version_info >= (3, 5)
 
-from pyspark.sql import SparkSession, functions as f, types as t
+from pyspark.sql import SparkSession, functions as f, types as t, Window
 
 def read_q_and_a_data(input_q, input_a):
     q_schema= t.StructType([
@@ -31,30 +31,57 @@ def read_q_and_a_data(input_q, input_a):
 
     a_df = spark.read.csv(input_a,
                       schema=a_schema,
-                        header=True,
-                        multiLine=True,
-                        escape='"',)
+                      header=True,
+                      multiLine=True,
+                      escape='"',)
     
     return q_df, a_df
 
+def get_cumsum(df, field, granularity=None):
+    total_field = df.groupBy().sum(field).head()[0]
+    df = df.where(df['owner_id'] != 'NA')
+    total_owners = df.count()
+    wdw = Window.orderBy(f.col(field).desc()).rowsBetween(Window.unboundedPreceding, Window.currentRow)
+    df = df.select(
+        (f.sum(field).over(wdw) / total_field).alias(f'{field}_fraction'),
+        (f.row_number().over(wdw) / total_owners).alias('percentile'),
+        f.row_number().over(wdw).alias('row')
+        )
+
+    if granularity:    
+         
+        breakpoints = {int (total_owners * i / (100 / granularity)) for i in range(1, 100)}
+        df = df.where(f.col('row').isin(breakpoints))
+
+    df = df.drop('row')
+    return df
+
 def main(input_q, input_a, output):
     q_df, a_df = read_q_and_a_data(input_q, input_a)
-    q_counts_by_user = q_df.groupBy('owner_id').count().withColumnRenamed('count', 'n_q')
-    a_counts_by_user = a_df.groupBy('owner_id').count().withColumnRenamed('count', 'n_a')
-    qa_counts_by_user = q_counts_by_user.join(a_counts_by_user, 'owner_id')
-    qa_counts_by_user.cache()
-    q_ranking = qa_counts_by_user.orderBy(f.desc('n_q'))
-    a_ranking = qa_counts_by_user.orderBy(f.desc('n_q'))
-    q_ranking.show()
-    a_ranking.show()
 
+    q_counts_by_user = q_df.groupBy('owner_id').count().withColumnRenamed('count', 'n_q')
+    q_sums = q_counts_by_user.groupBy('owner_id').agg(f.sum('n_q').alias('n_q'))
+    q_ranking = q_sums.orderBy(f.desc('n_q'))
+    q_ranking.cache()
+    q_cumsums = get_cumsum(q_ranking, 'n_q', 5)
+    q_cumsums.orderBy(f.desc('n_q')).show()
+
+    a_counts_by_user = a_df.groupBy('owner_id').count().withColumnRenamed('count', 'n_a')
+    a_sums = a_counts_by_user.groupBy('owner_id').agg(f.sum('n_a').alias('n_a'))
+    a_ranking = a_sums.orderBy(f.desc('n_a'))
+    a_ranking.cache()
+    a_cumsums = get_cumsum(a_ranking, 'n_a', 5)
+    a_cumsums.show()
+
+    # qa_counts_by_user = q_counts_by_user.join(a_counts_by_user, 'owner_id')
+    # print(qa_counts_by_user.corr('n_q', 'n_a'))
 
 if __name__ == '__main__':
     input_q = sys.argv[1]
     input_a = sys.argv[2]
     output = sys.argv[3]
     spark = SparkSession.builder.appName('Final Project: User Contribution').getOrCreate()
-    assert spark.version >= '3.0' 
+    assert spark.version >= '3.0'
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
     main(input_q, input_a, output)
