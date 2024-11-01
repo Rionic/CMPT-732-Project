8882353f459dee6730ec13ce6a88d7ac9826a015
Rajan Grewal <rajangrewal123@gmail.com>
2024-11-01 07:41:20 -0700

    update answer_speed to comply with new a_df and add overwrite on parquet output

--- answer_speed.py
+++ answer_speed.py
@@ -2,18 +2,19 @@ from pyspark.sql import SparkSession, functions as F
 import sys
 assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
 
-def calc_answer_speed(input_t_q, input_a):
-
-    t_q_df = spark.read.parquet(input_t_q)
+def calc_answer_speed(input_q, input_a):
+    q_df = spark.read.parquet(input_q)
     a_df = spark.read.parquet(input_a)
     
     # Rename columns and drop score for the incoming join
-    t_q_df = t_q_df.withColumnRenamed('Id', 'QuestionId'). \
+    q_df = q_df.withColumnRenamed('Id', 'QuestionId'). \
         withColumnRenamed('CreationDate', 'QuestionDate'). \
         drop('Score')
         
+    a_df = a_df.drop('Tag')
+        
     # Join t_q df with answers df to get t_q_a df
-    t_q_a_df = t_q_df.join(a_df, t_q_df['QuestionId'] == a_df['ParentId']). \
+    t_q_a_df = q_df.join(a_df, q_df['QuestionId'] == a_df['ParentId']). \
         select('QuestionId', 'Tag', 'QuestionDate', 'CreationDate', 'Score'). \
         withColumnRenamed('CreationDate', 'AnswerDate')
         
@@ -44,13 +45,13 @@ def calc_answer_speed(input_t_q, input_a):
     avg_answer_speed.show()
     
     # Write Results
-    avg_answer_speed.write.parquet(f'output/answer-speed')
+    avg_answer_speed.write.mode('overwrite').parquet('output/answer-speed')
 
 if __name__ == '__main__':
     input_path_tag_questions = sys.argv[1]
     input_path_answers = sys.argv[2]
     spark = SparkSession.builder.appName(
-        'Final Project: ETL').getOrCreate()
+        'Answer Speed').getOrCreate()
     assert spark.version >= '3.0'  # make sure we have Spark 3.0+
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
