4bcc83a7a6ae7288e7e68e2bb53e30904e6ab592
chhokara <chhokara@gmail.com>
2024-11-03 19:21:37 -0800

    minor adjustments

--- answer_speed.py
+++ answer_speed.py
@@ -2,7 +2,8 @@ from pyspark.sql import SparkSession, functions as F
 import sys
 assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
 
-def calc_answer_speed(input_q, input_a):
+
+def calc_answer_speed(input_q, input_a, output):
     q_df = spark.read.parquet(input_q)
     a_df = spark.read.parquet(input_a)
 
@@ -10,9 +11,9 @@ def calc_answer_speed(input_q, input_a):
     q_df = q_df.withColumnRenamed('Id', 'QuestionId'). \
         withColumnRenamed('CreationDate', 'QuestionDate'). \
         drop('Score')
-        
+
     a_df = a_df.drop('Tag')
-        
+
     # Join t_q df with answers df to get t_q_a df
     t_q_a_df = q_df.join(a_df, q_df['QuestionId'] == a_df['ParentId']). \
         select('QuestionId', 'Tag', 'QuestionDate', 'CreationDate', 'Score'). \
@@ -45,14 +46,16 @@ def calc_answer_speed(input_q, input_a):
     avg_answer_speed.show()
 
     # Write Results
-    avg_answer_speed.write.mode('overwrite').parquet('output/answer-speed')
+    avg_answer_speed.write.mode('overwrite').parquet(f'{output}/answer-speed')
+
 
 if __name__ == '__main__':
     input_path_tag_questions = sys.argv[1]
     input_path_answers = sys.argv[2]
+    output = sys.argv[3]
     spark = SparkSession.builder.appName(
         'Answer Speed').getOrCreate()
     assert spark.version >= '3.0'  # make sure we have Spark 3.0+
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
-    calc_answer_speed(input_path_tag_questions, input_path_answers)
+    calc_answer_speed(input_path_tag_questions, input_path_answers, output)
